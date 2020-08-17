5d07ef11fdb9455c2581700af3a0842d2a08c6e8
Joe Cui <joe.cui@bpanz.com>
2020-08-17 13:43:19 +1000

    Added always visible header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,24 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Route path="/" exact component={StreamList} />
-      <Route path="/streams/new" component={StreamCreate} />
-      <Route path="/streams/edit" component={StreamEdit} />
-      <Route path="/streams/delete" component={StreamDelete} />
-      <Route path="/streams/show" component={StreamShow} />
-    </BrowserRouter>
+    <div className="ui container">
+      <BrowserRouter>
+        <Header />
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/new" component={StreamCreate} />
+        <Route path="/streams/edit" component={StreamEdit} />
+        <Route path="/streams/delete" component={StreamDelete} />
+        <Route path="/streams/show" component={StreamShow} />
+      </BrowserRouter>
+    </div>
   );
 };
 
