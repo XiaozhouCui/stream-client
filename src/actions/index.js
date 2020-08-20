b4aa02d8245e7faa176917044f7c640f6d702107
Joe Cui <joe.cui@bpanz.com>
2020-08-20 11:56:41 +1000

    Added action creators for CRUD operations on streams

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import streams from "../apis/streams";
-import { SIGN_IN, SIGN_OUT } from "./types";
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM, FETCH_STREAMS, FETCH_STREAM, EDIT_STREAM, DELETE_STREAM } from "./types";
 
 export const signIn = (userId) => {
   return {
@@ -14,9 +14,40 @@ export const signOut = () => {
   };
 };
 
-// use redux-thunk and axios to handle createStream action
+// use axios and redux-thunk to handle HTTP requests
+
+// add a new stream
 export const createStream = (formValues) => {
   return async (dispatch) => {
-    streams.post("/streams", formValues); // axios instance with baseURL = localhost:3001
-  }
-}
+    // first send POST request to server
+    // "streams" is an axios instance with baseURL = localhost:3001
+    const response = await streams.post("/streams", formValues);
+    // after we got response, dispatch the action to reducer
+    dispatch({ type: CREATE_STREAM, payload: response.data });
+  };
+};
+
+// get all streams
+export const fetchStreams = () => async (dispatch) => {
+  const response = await streams.get("/streams");
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+// get a single stream
+export const fetchStream = (id) => async (dispatch) => {
+  const response = await streams.get(`/streams/${id}`);
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+// update a stream
+export const editStream = (id, formValues) => async (dispatch) => {
+  const response = await streams.put(`/streams/${id}`, formValues);
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+// delete a stream
+export const deleteStream = (id) => async (dispatch) => {
+  // delete request won't return any response
+  await streams.delete(`/streams/${id}`);
+  dispatch({ type: DELETE_STREAM, payload: id });
+};
