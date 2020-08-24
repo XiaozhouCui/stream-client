5b938d442aee60ce9819c9b72e69f3ff6039d8bf
Joe Cui <joe.cui@bpanz.com>
2020-08-24 14:55:22 +1000

    Added StreamShow component and hooked up to Redux

--- src/components/streams/StreamShow.jsx
+++ src/components/streams/StreamShow.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { fetchStream } from "../../actions/index";
+import { connect } from "react-redux";
 
 function StreamShow(props) {
+  useEffect(() => {
+    props.fetchStream(props.match.params.id);
+  }, []);
+
+  if (!props.stream) return <div>Loading...</div>
+
+  const { title, description } = props.stream;
+
   return (
     <div>
-      StreamShow
+      <h1>{title}</h1>
+      <h5>{description}</h5>
     </div>
   );
 }
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
